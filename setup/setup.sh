62c366e79253bb600141803c5356497006d6ec79
Anilkumar Kalyaner <anilskalyane@gmail.com>
2025-08-21 06:54:59 +0000

    upgraded to 8.14

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,7 +16,8 @@ BUNDLE_ZIP=$OUTPUT_DIR/bundle.zip
 CERT_KEYSTORES_ZIP=$OUTPUT_DIR/cert_keystores.zip
 HTTP_ZIP=$OUTPUT_DIR/http.zip
 
-yum install unzip openssl -y
+#yum install unzip openssl -y
+apt-get install -y unzip openssl
 
 create_self_signed_ca()
 {
